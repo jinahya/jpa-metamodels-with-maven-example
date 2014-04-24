f0430457aea318f71cf3619c446501c8829a2adc
Jin Kwon <onacit@gmail.com>
2014-04-24 14:49:28 +0900

    initial commit

---
+++ src/main/java/com/googlecode/jinahya/test/MyEntity.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2014 onacit.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.test;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+/**
+ * 
+ * @author <a href="mailto:onacit@gmail.com">Jin Kwon</a>
+ */
+@Entity
+public class MyEntity {
+
+
+    @Id
+    private Long id;
+
+}
